36bd94317b6ebceecee59c40e20d2cbb17d3e465
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-11 15:07:58 +1000

    Add initial source for July week 2 challenge 3

---
+++ java/src/main/java/julychallenge2020/week2/MultiLevelDoublyLinkedList.java
@@ -0,0 +1,18 @@
+package julychallenge2020.week2;
+
+// https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3386/
+public class MultiLevelDoublyLinkedList {
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+    };
+
+    class Solution {
+        public Node flatten(Node head) {
+            return new Node();
+        }
+    }
+}
---
+++ java/src/test/java/julychallenge2020/week2/MultiLevelDoublyLinkedListTest.java
@@ -0,0 +1,13 @@
+package julychallenge2020.week2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiLevelDoublyLinkedListTest {
+    @Test
+    public void test1() {
+
+
+    }
+}
