c379fe6645fc66bcbc505ebb717cbef19f369dbb
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-11 21:09:35 +1000

    Begin set up for memoization

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/subsets/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/subsets/Solution.java
@@ -5,29 +5,31 @@ import java.util.*;
 // Source: https://leetcode.com/explore/featured/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3387/
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> combinations = new ArrayList<>();
-        Integer combinationLength = nums.length-1;
-
         List<Integer> numsList = new ArrayList<>();
-        for(int n: nums) {
+        for(int n : nums) {
             numsList.add(n);
         }
+
 
         Set<List<Integer>> subsets = subsetsOf(numsList);
         Set<List<Integer>> totals = new HashSet<>();
 
         for(List<Integer> subset : subsets) {
-            int[] converted = new int[subset.size()];
-            for(int i = 0; i < converted.length; i++) {
-                converted[i] = subset.get(i);
-            }
-            totals.addAll(subsets(converted));
+            totals.addAll(subsets(convertListToArray(subset)));
         }
 
         totals.addAll(subsets);
         totals.add(numsList);
 
         return new ArrayList<>(totals);
+    }
+
+    public int[] convertListToArray(List<Integer> subset) {
+        int[] converted = new int[subset.size()];
+        for(int i = 0; i < converted.length; i++) {
+            converted[i] = subset.get(i);
+        }
+        return converted;
     }
 
     public Set<List<Integer>> subsetsOf(List<Integer> nums) {
