151a723330049d15f06022a6d4a7508941e3cc9d
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-12 12:00:33 +1000

    Initial implementation

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week1/arrangingcoins/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week1/arrangingcoins/Solution.java
@@ -3,6 +3,15 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week1.arrangingcoins;
 // https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3377/
 class Solution {
     public int arrangeCoins(int n) {
-        return -1;
+        int numFullStairCases = 0;
+
+        while(n > 0) {
+            for(int i = 0; i < numFullStairCases + 1; i++) {
+                n--;
+            }
+            numFullStairCases++;
+        }
+
+        return numFullStairCases - 1;
     }
 }
