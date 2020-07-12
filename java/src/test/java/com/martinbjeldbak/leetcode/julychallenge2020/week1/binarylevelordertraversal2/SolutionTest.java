cc088e15e412cefef6e6281fd2a3e0c54755b8a1
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-12 12:41:05 +1000

    Add indentation and docstring to tests

--- java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week1/binarylevelordertraversal2/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week1/binarylevelordertraversal2/SolutionTest.java
@@ -10,13 +10,26 @@ class SolutionTest {
     Solution subject = new Solution();
 
     @Test
+    // Given this binary tree
+    //    3
+    //   / \
+    //  9  20
+    //    /  \
+    //   15   7
+    // Its bottom up traversal order is
+    // [
+    //   [15,7],
+    //   [9,20],
+    //   [3]
+    // ]
     void testExample1() {
         TreeNode head = new TreeNode(3,
                 new TreeNode(9), new TreeNode(20,
-                new TreeNode(15), new TreeNode(7))
+                            new TreeNode(15), new TreeNode(7))
         );
 
-        assertEquals(List.of(List.of(15, 7), List.of(9, 20), List.of(3)), subject.levelOrderBottom(head));
+        assertEquals(List.of(List.of(15, 7), List.of(9, 20), List.of(3)),
+                     subject.levelOrderBottom(head));
     }
 
 }
