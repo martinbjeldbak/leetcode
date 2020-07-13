6728e8b376d2d1d4680cf72d7d2ef9c05fe5d79c
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-13 23:02:44 +1000

    Add initial code for integer reverse function

---
+++ java/src/main/java/com/martinbjeldbak/leetcode/problems/reverseinteger/Solution.java
@@ -0,0 +1,8 @@
+package com.martinbjeldbak.leetcode.problems.reverseinteger;
+
+// https://leetcode.com/problems/reverse-integer/
+class Solution {
+    public int reverse(int x) {
+        return -1;
+    }
+}
---
+++ java/src/test/java/com/martinbjeldbak/leetcode/problems/reverseinteger/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.martinbjeldbak.leetcode.problems.reverseinteger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    Solution subject = new Solution();
+
+    @Test
+    // Input: 123
+    // Output: 321
+    void testExample1() {
+        assertEquals(321, subject.reverse(123));
+    }
+
+    @Test
+    // Input: -123
+    // Output: -321
+    void testExample2() {
+        assertEquals(-321, subject.reverse(-123));
+    }
+
+    @Test
+    // Input: 120
+    // Output: 21
+    void testExample3() {
+        assertEquals(21, subject.reverse(120));
+    }
+
+}
