63d9bb2921b636202ac55136d8ee6c3f79845ce4
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-13 22:57:16 +1000

    Implement submitted solution

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/sametree/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/sametree/Solution.java
@@ -3,6 +3,18 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week2.sametree;
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3389/
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return true;
+        if(p == null && q != null)
+            return false;
+        if(p != null && q == null)
+            return false;
+        if(p == null && q == null) {
+            return true;
+        }
+
+        if(p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+
+        return false;
     }
 }
