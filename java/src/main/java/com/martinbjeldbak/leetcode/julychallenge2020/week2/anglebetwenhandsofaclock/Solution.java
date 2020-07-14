5edfee5135963e08a2ab6ec9a6d3ba959fc3265a
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-14 22:50:20 +1000

    First draft of solution for clock angle problem

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/anglebetwenhandsofaclock/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week2/anglebetwenhandsofaclock/Solution.java
@@ -3,6 +3,14 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week2.anglebetwenhandsofac
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/545/week-2-july-8th-july-14th/3390/
 class Solution {
     public double angleClock(int hour, int minutes) {
-        return -1;
+        if(hour == 12)
+            hour = 0;
+
+        int angleMinute = minutes * 6;
+        double minutePercentage = 1 / (360 / (double)angleMinute);
+        double angleHour = hour * 30 + (30 * minutePercentage);
+
+
+        return Math.abs(angleMinute - angleHour);
     }
 }
