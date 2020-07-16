c7f5c9f54e66912212e4e9958f6fdf7db25724e0
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-16 22:06:31 +1000

    Initial submitted solution

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
@@ -2,17 +2,18 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week3.pow;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.text.DecimalFormat;
 
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3392/
 public class Solution {
+    BigDecimal pow;
+
     public double myPow(double x, int n) {
         if(n < 0)
             return myPow(1 / x, -n);
         if(n == 1)
             return x;
 
-        BigDecimal pow = new BigDecimal(x * myPow(x, n - 1));
+        pow = new BigDecimal(x * myPow(x, n - 1));
 
         return pow.setScale(4, RoundingMode.HALF_EVEN).doubleValue();
     }
