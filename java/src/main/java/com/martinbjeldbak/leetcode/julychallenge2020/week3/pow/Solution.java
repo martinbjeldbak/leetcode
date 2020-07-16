f28a418260f59f0859ac5c3a90b58381a486e953
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-16 21:51:20 +1000

    Implement lazy solution using Math.pow

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
@@ -1,8 +1,15 @@
 package com.martinbjeldbak.leetcode.julychallenge2020.week3.pow;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3392/
 public class Solution {
     public double myPow(double x, int n) {
-        return -1;
+        double val = Math.pow(x, n);
+        BigDecimal t = new BigDecimal(val);
+
+        return t.setScale(4, RoundingMode.HALF_EVEN).doubleValue();
     }
 }
