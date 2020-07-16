732a94f022cddf9ddfd74bb96c07ac033606eb99
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-16 23:14:34 +1000

    Add accepted solution code

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
@@ -3,24 +3,39 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week3.pow;
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3392/
 public class Solution {
     public double myPow(double x, int n) {
+        long nLong = n;
         double pow;
 
         if(n == 0)
             return 1;
+        if(x == 1) {
+            return 1;
+        }
+        if(x == -1)
+            return x * (n < 0 ? -1 : 1);
 
         if(n < 0) {
             x = 1 / x;
-            n = -n;
+            nLong = -(long)n;
         }
 
         pow = x;
 
-        while(n > 1) {
+        while(nLong > 1) {
             pow *= x;
-            n--;
+
+            // Terminate early if we are in small digits
+            if(truncateDecimals(pow) == 0)
+                return 0;
+
+            nLong--;
         }
 
         // Do this conversion for 4 decimal digit truncation
-        return (long)(pow * 100000) / 100000.0;
+        return truncateDecimals(pow);
+    }
+
+    private double truncateDecimals(double val) {
+        return (long)(val * 100000) / 100000.0;
     }
 }
--- java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/SolutionTest.java
@@ -28,6 +28,11 @@ class SolutionTest {
     }
 
     @Test
+    void testSlow2() {
+        assertEquals(1, subject.myPow(1, 2147483647));
+    }
+
+    @Test
     void testExampleRounding() {
         assertEquals(700.28148, subject.myPow(8.88023, 3));
     }
@@ -40,5 +45,25 @@ class SolutionTest {
     @Test
     void testNOne() {
         assertEquals(0.44528, subject.myPow(0.44528, 1));
+    }
+
+    @Test
+    void testZero2() {
+        assertEquals(0, subject.myPow(2, -2147483648));
+    }
+
+    @Test
+    void testEdgecase1() {
+        assertEquals(-1, subject.myPow(-1, 2147483647));
+    }
+
+    @Test
+    void testEdgecase2() {
+        assertEquals(1, subject.myPow(-1, -2147483648));
+    }
+
+    @Test
+    void testEdgecase3() {
+        assertEquals(1, subject.myPow(1, -2147483648));
     }
 }
