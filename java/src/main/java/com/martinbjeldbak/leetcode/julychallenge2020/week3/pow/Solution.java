5ddd781aad40a0e37dd9142cae0b791c7baa5b66
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-16 21:57:19 +1000

    Implement initial recursive solution

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week3/pow/Solution.java
@@ -7,9 +7,13 @@ import java.text.DecimalFormat;
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/546/week-3-july-15th-july-21st/3392/
 public class Solution {
     public double myPow(double x, int n) {
-        double val = Math.pow(x, n);
-        BigDecimal t = new BigDecimal(val);
+        if(n < 0)
+            return myPow(1 / x, -n);
+        if(n == 1)
+            return x;
 
-        return t.setScale(4, RoundingMode.HALF_EVEN).doubleValue();
+        BigDecimal pow = new BigDecimal(x * myPow(x, n - 1));
+
+        return pow.setScale(4, RoundingMode.HALF_EVEN).doubleValue();
     }
 }
