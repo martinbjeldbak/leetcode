795f4da6a7424e98e83088fc59e0e998c4efa540
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-27 22:56:29 +1000

    Initial submitted and accepted solution w/ tests

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week4/constructbinarytreefrominorderandpostordertraversal/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week4/constructbinarytreefrominorderandpostordertraversal/Solution.java
@@ -1,8 +1,31 @@
 package com.martinbjeldbak.leetcode.julychallenge2020.week4.constructbinarytreefrominorderandpostordertraversal;
 
+import java.util.Arrays;
+
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/547/week-4-july-22nd-july-28th/3403/
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return new TreeNode();
+        if(postorder.length == 0) {
+            return null;
+        }
+
+        int rootVal = postorder[postorder.length - 1];
+        int[] leftSubtreeInOrder, rightSubtreeInOrder, leftSubtreePostOrder, rightSubtreePostOrder;
+
+        for(int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == rootVal) {
+                leftSubtreeInOrder = Arrays.copyOfRange(inorder, 0, i);
+                rightSubtreeInOrder = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+
+                leftSubtreePostOrder = Arrays.copyOfRange(postorder, 0, leftSubtreeInOrder.length);
+                rightSubtreePostOrder = Arrays.copyOfRange(postorder, leftSubtreePostOrder.length, postorder.length - 1);
+
+                return new TreeNode(rootVal, buildTree(leftSubtreeInOrder, leftSubtreePostOrder),
+                                             buildTree(rightSubtreeInOrder, rightSubtreePostOrder));
+            }
+        }
+
+        // Should not go here
+        return null;
     }
 }
--- java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week4/constructbinarytreefrominorderandpostordertraversal/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week4/constructbinarytreefrominorderandpostordertraversal/SolutionTest.java
@@ -1,5 +1,6 @@
 package com.martinbjeldbak.leetcode.julychallenge2020.week4.constructbinarytreefrominorderandpostordertraversal;
 
+import com.sun.source.tree.Tree;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +9,37 @@ class SolutionTest {
     Solution subject = new Solution();
 
     @Test
+    // For example, given
+    //
+    // inorder = [9,3,15,20,7]
+    // postorder = [9,15,7,20,3]
+    // Return the following binary tree:
+    //
+    //    3
+    //   / \
+    //  9  20
+    //    /  \
+    //   15   7
     void testExample1() {
         TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
         assertEquals(root, subject.buildTree(new int[] { 9, 3, 15, 20, 7 }, new int[] { 9, 15, 7, 20, 3 }));
+    }
+
+    @Test
+    void testMartinExample1() {
+        TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(4, null, new TreeNode(8)), new TreeNode(5)), new TreeNode(3, new TreeNode(6), new TreeNode(7)));
+        assertEquals(root, subject.buildTree(new int[] { 4, 8, 2, 5, 1, 6, 3, 7 }, new int[] { 8, 4, 5, 2, 6, 7, 3, 1 }));
+    }
+
+    @Test
+    void testMartinExample2() {
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        assertEquals(root, subject.buildTree(new int[] { 2, 1, 3 }, new int[] { 2, 3, 1 }));
+    }
+
+    @Test
+    void testMartinExample3() {
+        TreeNode root = new TreeNode(1);
+        assertEquals(root, subject.buildTree(new int[] { 1 }, new int[] { 1 }));
     }
 }
