5befb3aac58af2e8d957e7c6665e15322b693029
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-29 23:01:43 +1000

    Implement solution after researching state transition solution

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/besttimetobuyandsellstock/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/besttimetobuyandsellstock/Solution.java
@@ -1,27 +1,25 @@
 package com.martinbjeldbak.leetcode.julychallenge2020.week5.besttimetobuyandsellstock;
 
-import java.util.Arrays;
-
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3405/
 class Solution {
     public int maxProfit(int[] prices) {
-        return profitAt(0, prices);
-    }
-
-    private static int profitAt(int quantity, int[] prices) {
-
-        if(prices.length == 0)
+        if(prices.length <= 1)
             return 0;
-        // buy
-        int buyProfit = -prices[0] + profitAt(quantity + 1, Arrays.copyOfRange(prices, 1, prices.length));
-        // sell
-        int sellProfit;
-        if(quantity > 0) {
-             sellProfit = quantity * prices[0] + profitAt(0, Arrays.copyOfRange(prices, 2 > prices.length ? 1 : 2, prices.length));
-        } else {
-            sellProfit = 0;
+
+        int[] s0 = new int[prices.length];
+        int[] s1 = new int[prices.length];
+        int[] s2 = new int[prices.length];
+
+        s0[0] = 0; // if we rest
+        s1[0] = -prices[0]; // if we buy
+        s2[0] = Integer.MIN_VALUE; // cool state after selling
+
+        for(int i = 1; i < prices.length; i++) {
+            s0[i] = Math.max(s0[i - 1], s2[i - 1]);
+            s1[i] = Math.max(s0[i - 1] - prices[i], s1[i - 1]);
+            s2[i] = s1[i - 1] + prices[i];
         }
 
-        return Math.max(buyProfit, sellProfit);
+        return Math.max(s0[prices.length - 1], s2[prices.length - 1]);
     }
 }
--- java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/besttimetobuyandsellstock/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/besttimetobuyandsellstock/SolutionTest.java
@@ -11,4 +11,21 @@ class SolutionTest {
     void testExample1() {
         assertEquals(3, subject.maxProfit(new int[] { 1, 2, 3, 0, 2 }));
     }
+
+    @Test
+    void testMartinExample() {
+        assertEquals(1, subject.maxProfit(new int[] { 1, 2 }));
+    }
+
+    @Test
+    // buy, buy, sell
+    void testMartinExample2() {
+        assertEquals(10, subject.maxProfit(new int[] { 1, 0, 10 }));
+    }
+
+    @Test
+        // buy, buy, sell
+    void testMartinExample3() {
+        assertEquals(3, subject.maxProfit(new int[] { 1, 2, 4 }));
+    }
 }
