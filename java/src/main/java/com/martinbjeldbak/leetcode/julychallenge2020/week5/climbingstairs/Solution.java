e54b208a3450513b6211d996a4098edbcd285695
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-31 22:21:53 +1000

    Collapse if statement

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
@@ -3,11 +3,8 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week5.climbingstairs;
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3407/
 class Solution {
     public int climbStairs(int n) {
-        if(n == 1) {
-            return 1;
-        }
-        if(n == 2) {
-            return 2;
+        if(n == 1 || n == 2) {
+            return n;
         }
 
         return climbStairs(n - 1) + climbStairs(n - 2);
