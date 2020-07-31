cb138bdf49b5959fad77e7b81a132349824c1718
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-07-31 22:21:27 +1000

    Initial slow solution

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
@@ -3,6 +3,13 @@ package com.martinbjeldbak.leetcode.julychallenge2020.week5.climbingstairs;
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3407/
 class Solution {
     public int climbStairs(int n) {
-        return -1;
+        if(n == 1) {
+            return 1;
+        }
+        if(n == 2) {
+            return 2;
+        }
+
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 }
--- java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/SolutionTest.java
+++ java/src/test/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/SolutionTest.java
@@ -8,12 +8,39 @@ class SolutionTest {
     Solution subject = new Solution();
 
     @Test
+    // There are two ways to climb to the top.
+    // 1. 1 step + 1 step
+    // 2. 2 steps
     void testExample1() {
         assertEquals(2, subject.climbStairs(2));
     }
 
     @Test
+    // There are three ways to climb to the top.
+    // 1. 1 step + 1 step + 1 step
+    // 2. 1 step + 2 steps
+    // 3. 2 steps + 1 step
     void testExample2() {
         assertEquals(3, subject.climbStairs(3));
+    }
+
+    @Test
+    void testMartinExample() {
+        assertEquals(1, subject.climbStairs(1));
+    }
+
+    @Test
+    // 1. 1 step + 1 step + 1 step + 1 step
+    // 2. 1 step + 2 steps + 1 step + 1 step
+    // 3. 1 step + 1 step + 2 steps
+    // 4. 2 steps + 2 steps
+    // 5. 2 steps + 1 step + 1 step
+    void testMartinExample2() {
+        assertEquals(5, subject.climbStairs(4));
+    }
+
+    @Test
+    void testMartinSlow() {
+        assertEquals(1836311903, subject.climbStairs(45));
     }
 }
