b8eb29c57ed0828244313da3a7f29ff0215822b3
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-08-01 18:49:51 +1000

    Memoize climbing stairs problem

--- java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/julychallenge2020/week5/climbingstairs/Solution.java
@@ -1,12 +1,21 @@
 package com.martinbjeldbak.leetcode.julychallenge2020.week5.climbingstairs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://leetcode.com/explore/challenge/card/july-leetcoding-challenge/548/week-5-july-29th-july-31st/3407/
 class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+
     public int climbStairs(int n) {
         if(n == 1 || n == 2) {
             return n;
         }
 
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        if(!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n - 1) + climbStairs(n - 2));
+        }
+
+        return memo.get(n);
     }
 }
