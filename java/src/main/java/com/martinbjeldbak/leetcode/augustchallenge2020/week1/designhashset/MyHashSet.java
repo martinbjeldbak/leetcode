5ef26b43d724f05781dc9d67b536095d721771cc
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-08-02 22:52:20 +1000

    Implement dumb and passing solution

--- java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week1/designhashset/MyHashSet.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week1/designhashset/MyHashSet.java
@@ -1,22 +1,22 @@
 package com.martinbjeldbak.leetcode.augustchallenge2020.week1.designhashset;
 
 class MyHashSet {
+    boolean[] bucket = new boolean[1000000];
 
     /** Initialize your data structure here. */
     public MyHashSet() {
-
     }
 
     public void add(int key) {
-        Integer.hashCode(key);
+        bucket[key] = true;
     }
 
     public void remove(int key) {
-
+        bucket[key] = false;
     }
 
     /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-
+        return bucket[key];
     }
 }
