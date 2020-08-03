830978c15e2fce435199451d49cf367c87cd8f4f
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-08-03 22:01:21 +1000

    Improve regex for stripping of non-alphanumeric characters

--- java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week1/validpalindrome/Solution.java
+++ java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week1/validpalindrome/Solution.java
@@ -3,7 +3,7 @@ package com.martinbjeldbak.leetcode.augustchallenge2020.week1.validpalindrome;
 // https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/549/week-1-august-1st-august-7th/3411/
 class Solution {
     public boolean isPalindrome(String s) {
-        String loweredS = s.toLowerCase().replaceAll("\\W", "");
+        String loweredS = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         int sLength = loweredS.length();
 
         for(int i = 0; i < sLength / 2; i++) {
