ad89aaeb2026ccdf49df0b24527fc7c3bc012d98
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-08-08 18:24:09 +1000

    Initial code and failing spec for Path Sum III problem
    https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3417/

---
+++ java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week2/pathsumiii/Solution.java
@@ -0,0 +1,8 @@
+package com.martinbjeldbak.leetcode.augustchallenge2020.week2.pathsumiii;
+
+// https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3417/
+class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        return -1;
+    }
+}
