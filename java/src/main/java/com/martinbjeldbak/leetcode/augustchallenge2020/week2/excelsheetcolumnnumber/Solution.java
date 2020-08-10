9cba24428366d76645aad011c20038ffd052a8e9
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-08-10 22:28:38 +1000

    Initial solution passing specs for excel problem
    https://leetcode.com/explore/featured/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3419/

---
+++ java/src/main/java/com/martinbjeldbak/leetcode/augustchallenge2020/week2/excelsheetcolumnnumber/Solution.java
@@ -0,0 +1,24 @@
+package com.martinbjeldbak.leetcode.augustchallenge2020.week2.excelsheetcolumnnumber;
+
+import java.util.*;
+
+// https://leetcode.com/explore/featured/card/august-leetcoding-challenge/550/week-2-august-8th-august-14th/3419/
+class Solution {
+    public int titleToNumber(String s) {
+        List<Character> alphabet = new ArrayList<>(List.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'));
+
+        int sum = 0;
+        int offset;
+
+        for(int i = 0; i < s.length(); i++) {
+            if((s.length() - i - 1) == 0)
+                offset = alphabet.indexOf(s.charAt(i)) + 1;
+            else
+                offset = (26 * (s.length() - i - 1)) * (alphabet.indexOf(s.charAt(i)) + 1);
+
+            sum += offset;
+        }
+
+        return sum;
+    }
+}
---
+++ java/src/test/java/com/martinbjeldbak/leetcode/augustchallenge2020/week2/excelsheetcolumnnumber/SolutionTest.java
@@ -0,0 +1,50 @@
+package com.martinbjeldbak.leetcode.augustchallenge2020.week2.excelsheetcolumnnumber;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    Solution subject = new Solution();
+
+    @Test
+    void testExample1() {
+        assertEquals(1, subject.titleToNumber("A"));
+    }
+
+    @Test
+    void testExample2() {
+        assertEquals(28, subject.titleToNumber("AB"));
+    }
+
+    @Test
+    void testExample3() {
+        assertEquals(701, subject.titleToNumber("ZY"));
+    }
+
+    @Test
+    void testExample4() {
+        // TODO! max input
+        assertEquals(8239, subject.titleToNumber("FXSHRXW"));
+    }
+
+    @Test
+    void testMartinExample1() {
+        assertEquals(44, subject.titleToNumber("AR"));
+    }
+
+    @Test
+    void testMartinExample2() {
+        assertEquals(79, subject.titleToNumber("CA"));
+    }
+
+    @Test
+    void testMartinExample3() {
+        assertEquals(52, subject.titleToNumber("AZ"));
+    }
+
+    @Test
+    void testMartinExample4() {
+        assertEquals(53, subject.titleToNumber("BA"));
+    }
+}
