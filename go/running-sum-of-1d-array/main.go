821ddbab0a7f56061bfc2a13e6db307e6599d510
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2022-06-26 12:58:48 +0000

    Implement running sum

--- go/running-sum-of-1d-array/main.go
+++ go/running-sum-of-1d-array/main.go
@@ -1,5 +1,13 @@
 package main
 
 func runningSum(nums []int) []int {
-	return []int{}
+	runningSum := make([]int, len(nums))
+	curSum := 0
+
+	for i := 0; i < len(nums); i++ {
+		curSum += nums[i]
+		runningSum[i] = curSum
+	}
+
+	return runningSum
 }
