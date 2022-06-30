079ef9ca304faa1681b61ace2deb65ea9ec32187
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2022-06-30 09:49:11 +0000

    Kadan'es algorithm

--- go/best-time-to-buy-and-sell-stock/main.go
+++ go/best-time-to-buy-and-sell-stock/main.go
@@ -1,22 +1,18 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 package besttimetobuyandsellstock
 
+import "math"
+
 func maxProfit(prices []int) int {
 	maximumProfit := 0
+	minimumProfit := math.MaxInt
 
 	for i := 0; i < len(prices); i++ {
-		boughtPrice := prices[i]
-
-		for j := i; j < len(prices); j++ {
-			sellPrice := prices[j]
-
-			potentialProfit := sellPrice - boughtPrice
-
-			if potentialProfit > maximumProfit {
-				maximumProfit = potentialProfit
-			}
+		if prices[i] < minimumProfit {
+			minimumProfit = prices[i]
+		} else if prices[i] > minimumProfit {
+			maximumProfit = int(math.Max(float64(prices[i])-float64(minimumProfit), float64(maximumProfit)))
 		}
-
 	}
 	return maximumProfit
 }
