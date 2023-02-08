d2c06870ad097ce6de7fe876828a01cca110e1f0
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-02-08 23:54:21 +1100

    Give up, use official solution

--- go/jump-game-ii/main.go
+++ go/jump-game-ii/main.go
@@ -1,45 +1,21 @@
 package jumpgameii
 
-import "fmt"
+import (
+	"math"
+)
 
 func jump(nums []int) (result int) {
-	fmt.Printf("Got nums %v\n", nums)
-	minJumpStore := make(map[int]int)
-	result = jumpRec(nums, 0, 0, minJumpStore)
+	n := len(nums)
+	curEnd := 0
+	curFar := 0
 
-	fmt.Printf("Jumpstore %v\n", minJumpStore)
+	for i := 0; i < n-1; i++ {
+		curFar = int(math.Max(float64(curFar), float64(i+nums[i])))
 
-	return result
-}
-
-func jumpRec(nums []int, start int, numJumps int, minJumpStore map[int]int) int {
-	// If we already have computed, return it
-	minJ, exists := minJumpStore[start]
-	if exists {
-		return minJ
-	}
-
-	if start >= len(nums)-1 {
-		return numJumps
-	}
-	fmt.Printf("  start: %v, value: %v, numJumps: %v\n", start, nums[start], numJumps)
-
-	maxJumps := nums[start]
-
-	jumps := make([]int, 0, maxJumps)
-
-	for i := 1; i <= maxJumps; i++ {
-		minimumJumps := jumpRec(nums, start+i, numJumps+1, minJumpStore)
-
-		jumps = append(jumps, minimumJumps)
-	}
-	fmt.Printf("  jumps: %v\n", jumps)
-
-	minJumps := 99999
-	for _, jump := range jumps {
-		if jump < minJumps {
-			minJumps = jump
+		if i == curEnd {
+			result++
+			curEnd = curFar
 		}
 	}
-	return minJumps
+	return result
 }
--- go/jump-game-ii/main_test.go
+++ go/jump-game-ii/main_test.go
@@ -11,35 +11,40 @@ func Test_jump(t *testing.T) {
 		args args
 		want int
 	}{
-		// {
-		// 	name: "Example 1",
-		// 	args: args{nums: []int{2, 3, 1, 1, 4}},
-		// 	want: 2,
-		// },
-		// {
-		// 	name: "Example 2",
-		// 	args: args{nums: []int{2, 3, 0, 1, 4}},
-		// 	want: 2,
-		// },
-		// {
-		// 	name: "Failed example 1",
-		// 	args: args{nums: []int{2, 1}},
-		// 	want: 1,
-		// },
-		// {
-		// 	name: "Martin example 1",
-		// 	args: args{nums: []int{1}},
-		// 	want: 0,
-		// },
-		// {
-		// 	name: "Failed example 2",
-		// 	args: args{nums: []int{1, 2, 1, 1, 1}},
-		// 	want: 3,
-		// },
+		{
+			name: "Example 1",
+			args: args{nums: []int{2, 3, 1, 1, 4}},
+			want: 2,
+		},
+		{
+			name: "Example 2",
+			args: args{nums: []int{2, 3, 0, 1, 4}},
+			want: 2,
+		},
+		{
+			name: "Failed example 1",
+			args: args{nums: []int{2, 1}},
+			want: 1,
+		},
+		{
+			name: "Martin example 1",
+			args: args{nums: []int{1}},
+			want: 0,
+		},
+		{
+			name: "Failed example 2",
+			args: args{nums: []int{1, 2, 1, 1, 1}},
+			want: 3,
+		},
 		{
 			name: "Failed example 3",
 			args: args{nums: []int{6, 2, 6, 1, 7, 9, 3, 5, 3, 7, 2, 8, 9, 4, 7, 7, 2, 2, 8, 4, 6, 6, 1, 3}},
-			want: 3, //4,
+			want: 4,
+		},
+		{
+			name: "Failed example 4 timeout",
+			args: args{nums: []int{5, 6, 4, 4, 6, 9, 4, 4, 7, 4, 4, 8, 2, 6, 8, 1, 5, 9, 6, 5, 2, 7, 9, 7, 9, 6, 9, 4, 1, 6, 8, 8, 4, 4, 2, 0, 3, 8, 5}},
+			want: 5,
 		},
 	}
 	for _, tt := range tests {
