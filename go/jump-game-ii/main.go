8af2e164f8b16473192bcc4af39e98ce24b0f86d
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-02-08 22:57:49 +1100

    Get skeleton for dynamic programming

--- go/jump-game-ii/main.go
+++ go/jump-game-ii/main.go
@@ -2,12 +2,23 @@ package jumpgameii
 
 import "fmt"
 
-func jump(nums []int) int {
+func jump(nums []int) (result int) {
 	fmt.Printf("Got nums %v\n", nums)
-	return jumpRec(nums, 0, 0)
+	minJumpStore := make(map[int]int)
+	result = jumpRec(nums, 0, 0, minJumpStore)
+
+	fmt.Printf("Jumpstore %v\n", minJumpStore)
+
+	return result
 }
 
-func jumpRec(nums []int, start int, numJumps int) int {
+func jumpRec(nums []int, start int, numJumps int, minJumpStore map[int]int) int {
+	// If we already have computed, return it
+	minJ, exists := minJumpStore[start]
+	if exists {
+		return minJ
+	}
+
 	if start >= len(nums)-1 {
 		return numJumps
 	}
@@ -15,10 +26,12 @@ func jumpRec(nums []int, start int, numJumps int) int {
 
 	maxJumps := nums[start]
 
-	jumps := make([]int, 0, len(nums))
+	jumps := make([]int, 0, maxJumps)
 
 	for i := 1; i <= maxJumps; i++ {
-		jumps = append(jumps, jumpRec(nums, start+i, numJumps+1))
+		minimumJumps := jumpRec(nums, start+i, numJumps+1, minJumpStore)
+
+		jumps = append(jumps, minimumJumps)
 	}
 	fmt.Printf("  jumps: %v\n", jumps)
 
