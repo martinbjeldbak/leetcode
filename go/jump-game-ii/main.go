7f1132b4182e45a1d5afcef6dde9b29221baa59e
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2023-02-08 21:28:11 +1100

    Initial greedy solution

---
+++ go/jump-game-ii/main.go
@@ -0,0 +1,32 @@
+package jumpgameii
+
+import "fmt"
+
+func jump(nums []int) int {
+	fmt.Printf("Got nums %v\n", nums)
+	return jumpRec(nums, 0, 0)
+}
+
+func jumpRec(nums []int, start int, numJumps int) int {
+	fmt.Printf("  start: %v, value: %v, numJumps: %v\n", start, nums[start], numJumps)
+	if start == len(nums)-1 {
+		return numJumps
+	}
+
+	maxJumps := nums[start]
+
+	jumps := make([]int, 0, len(nums))
+
+	for i := 1; i <= maxJumps; i++ {
+		jumps = append(jumps, jumpRec(nums, start+i, numJumps+1))
+	}
+	fmt.Printf("  jumps: %v\n", jumps)
+
+	minJumps := 99999
+	for _, jump := range jumps {
+		if jump < minJumps {
+			minJumps = jump
+		}
+	}
+	return minJumps
+}
